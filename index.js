d1aed45264a1dd55b63d5bf0ade57f91e2749bd2
harrietrowan01 <harrietrowan01@gmail.com>
2021-07-01 04:32:40 -0400

    updated json

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const URL = ''
+const URL = 'http://localhost:3000/villagers';
 
 fetch(URL)
 .then(response => response.json())
 .then(data => {
-    console.log(data)
-    
+    console.log(data);
+    const ul = document.querySelector('#villager-names');
+    data.forEach(villager => {
+      const intervillagers = `${villager.id} ${villager.name}`;
+        ul.append(intervillagers);
+
+    })
 
 })
