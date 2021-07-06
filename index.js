9a538d4fc830425a7484e361f3ceff8aae1bf139
harrietrowan01 <harrietrowan01@gmail.com>
2021-07-06 16:26:00 -0400

    new idea changes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const URL = 'http://localhost:3000/villagers';
+const URL = 'http://localhost:3000/hunters';
 
 fetch(URL)
 .then(response => response.json())
 .then(data => {
     console.log(data);
-    const ul = document.querySelector('#villager-names');
-    data.forEach(villager => {
-      const intervillagers = `${villager.id} ${villager.name}`;
-        ul.append(intervillagers);
+    const ul = document.querySelector('#hunter-names');
+    data.forEach(hunter => {
+      const interhunters = `${hunter.id} ${hunter.name}`;
+        ul.append(interhunters);
 
     })
 
