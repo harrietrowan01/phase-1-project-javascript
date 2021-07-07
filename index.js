2360f0491d848eaaac9232acce432e7dbe8dc999
harrietrowan01 <harrietrowan01@gmail.com>
2021-07-07 16:18:23 -0400

    final code

--- index.js
+++ index.js
@@ -1,33 +1,55 @@
 const URL = 'http://localhost:3000/hunters';
-
+let hunters = []
 fetch(URL)
 .then(response => response.json())
 .then(data => {
   console.log(data);
+  hunters = data
   const ul = document.querySelector('#hunter-names');
   data.forEach(hunter => {
-    const interhunters = `${hunter.id} ${hunter.name} ${hunter.specialty}`;
+    const interhunters = `${hunter.id}. ${hunter.name}, ${hunter.specialty}`;
     const li = document.createElement('li')
     li.textContent = interhunters
     ul.append(li);
   })
 })
 
-function holdTheGlass () {
-  const ul = document.querySelector('#hunter-names');
-  ul.forEach(hunter => {
-    if hunters.category == "Enhancer"
-    "The water is overflowing!"
-    elsif hunters.category == "Transmuter"
-    "The water tastes different!"
-    elsif hunters.category == "Conjurer"
-    "There is something forming in the water!"
-    elsif hunters.category == "Manipulator"
-    "The leaf is moving!"
-    elsif hunters.category == "Emitter"
-    "The water is changing colors!"
-    else 
-    "Something is unexpected is happening to the water!"
-  })
-}
+function holdTheGlass (event) {
+  console.log(hunters)
+  const name = event.target.id
+  console.log(name)
+  function checkName (hunter) {
+    return hunter.name === name
+  }
+  const theFoundHunter = hunters.find(checkName)
+  console.log(theFoundHunter)
+    if (theFoundHunter.category == "Enhancer") {
+    alert ("The water is overflowing!")
+    }
+    else if (theFoundHunter.category == "Transmuter") {
+    alert ("The water tastes different!")
+    }
+    else if (theFoundHunter.category == "Conjurer") {
+    alert ("There is something forming in the water!")
+    }
+    else if (theFoundHunter.category == "Manipulator") {
+    alert ("The leaf is moving!")
+    }
+    else if (theFoundHunter.category == "Emitter") {
+    alert ("The water is changing colors!")
+    }
+    else {
+    alert ("Something unexpected is happening to the water!")
+    }
+  }
 
+
+document.getElementById("Gon").addEventListener("click", holdTheGlass);
+
+document.getElementById("Killua").addEventListener("click", holdTheGlass);
+
+document.getElementById("Hisoka").addEventListener("click", holdTheGlass);
+
+document.getElementById("Illumi").addEventListener("click", holdTheGlass);
+
+document.getElementById("Chrollo").addEventListener("click", holdTheGlass);
