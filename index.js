737774aac2126c2032549f6e2fc5dd30c20f8a63
harrietrowan01 <harrietrowan01@gmail.com>
2021-07-06 21:08:19 -0400

    save for the night

--- index.js
+++ index.js
@@ -17,18 +17,17 @@ fetch(URL)
     const ul =document.querySelector('#hunter-names');
     const interhunters = `${hunter.id} ${hunter.name} ${hunter.category}
     if (interhunters.category == Enhancer)
-    alert ("The water is overflowing!")
+    "The water is overflowing!"
     elseif (interhunters.category == Transmuter)
-    alert ("This water tastes funny!")
+    "This water tastes funny!"
     elsif (interhunter.category == Conjurer)
-    alert ("There's something in the water!")
+    "There's something in the water!"
     elsif (interhunter.category == Manipulator)
-    alert ("The leaf is moving!")
+    "The leaf is moving!"
     elsif (interhunter.category == Emitter)
-    alert ("The water is changing colors!")
+    "The water is changing colors!"
     else 
-    alert ("What is happening to the water? Must be a Specialist.") 
+    "What is happening to the water? Must be a Specialist." 
   }
+  
   document.getElementById("Gon").addEventListener("click", holdTheGlass());
-
-})
