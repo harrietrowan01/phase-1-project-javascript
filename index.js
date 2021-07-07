b73d45c9e192a0f0d4c0d4c60542862fa42e570d
harrietrowan01 <harrietrowan01@gmail.com>
2021-07-07 13:13:08 -0400

    updated styling and functions

--- index.js
+++ index.js
@@ -3,31 +3,31 @@ const URL = 'http://localhost:3000/hunters';
 fetch(URL)
 .then(response => response.json())
 .then(data => {
-    console.log(data);
-    const ul = document.querySelector('#hunter-names');
-    data.forEach(hunter => {
-      const interhunters = `${hunter.id} ${hunter.name}`;
-      const li = document.createElement('li')
-      li.textContent = interhunters
-        ul.append(li);
+  console.log(data);
+  const ul = document.querySelector('#hunter-names');
+  data.forEach(hunter => {
+    const interhunters = `${hunter.id} ${hunter.name} ${hunter.specialty}`;
+    const li = document.createElement('li')
+    li.textContent = interhunters
+    ul.append(li);
+  })
+})
 
-    })
-  
-  function holdTheGlass() {
-    const ul =document.querySelector('#hunter-names');
-    const interhunters = `${hunter.id} ${hunter.name} ${hunter.category}
-    if (interhunters.category == Enhancer)
+function holdTheGlass () {
+  const ul = document.querySelector('#hunter-names');
+  ul.forEach(hunter => {
+    if hunters.category == "Enhancer"
     "The water is overflowing!"
-    elseif (interhunters.category == Transmuter)
-    "This water tastes funny!"
-    elsif (interhunter.category == Conjurer)
-    "There's something in the water!"
-    elsif (interhunter.category == Manipulator)
+    elsif hunters.category == "Transmuter"
+    "The water tastes different!"
+    elsif hunters.category == "Conjurer"
+    "There is something forming in the water!"
+    elsif hunters.category == "Manipulator"
     "The leaf is moving!"
-    elsif (interhunter.category == Emitter)
+    elsif hunters.category == "Emitter"
     "The water is changing colors!"
     else 
-    "What is happening to the water? Must be a Specialist." 
-  }
-  
-  document.getElementById("Gon").addEventListener("click", holdTheGlass());
+    "Something is unexpected is happening to the water!"
+  })
+}
+
